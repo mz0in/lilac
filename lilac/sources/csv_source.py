99cbf19eda8c36415d814285b475cf446c029436
Nikhil Thorat <nikhil@lilacml.com>
2024-01-08 12:03:17 -0500

    Fix a bug with CSV source reader for TSV files, and named columns. (#1040)
    Towards https://github.com/lilacai/lilac/issues/1018

--- lilac/sources/csv_source.py
+++ lilac/sources/csv_source.py
@@ -18,9 +18,12 @@ LINE_NUMBER_COLUMN = '__line_number__'
 
 
 class CSVSource(Source):
-  """CSV data loader
+  r"""CSV or TSV data loader
 
-  CSV files can live locally as a filepath, point to an external URL, or live on S3, GCS, or R2.
+  CSV or TSV files can live locally as a filepath, point to an external URL, or live on S3, GCS, or
+  R2.
+
+  For TSV files, use `\t` as the delimiter.
 
   For more details on authorizing access to S3, GCS or R2, see:
   https://duckdb.org/docs/guides/import/s3_import.html
@@ -32,10 +35,12 @@ class CSVSource(Source):
     description='A list of paths to CSV files. '
     'Paths can be local, point to an HTTP(s) url, or live on GCS, S3 or R2.'
   )
-  delim: Optional[str] = Field(default=',', description='The CSV file delimiter to use.')
+  delim: Optional[str] = Field(
+    default=',', description='The CSV file delimiter to use. For TSV files, use `\\t`.'
+  )
   header: Optional[bool] = Field(default=True, description='Whether the CSV file has a header row.')
-  names: Optional[list[str]] = Field(
-    default=None, description='Provide header names if the file does not contain a header.'
+  names: list[str] = Field(
+    default=[], description='Provide header names if the file does not contain a header.'
   )
 
   _source_schema: Optional[SourceSchema] = None
